a3439c71d5fdc1af3bbd7ddff0a93a523c312868
Alex McMillan <67392937+alexmcmillan86@users.noreply.github.com>
2020-09-02 13:51:50 -0400

    Update application.py

--- application.py
+++ application.py
@@ -27,13 +27,13 @@ def after_request(response):
     return response
 
 # Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
+#app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("postgres://ntcsyiawhlvfdx:9b862051f17493d8a1a62df4ca635bde468fab3e728fcdb2909b8c46741e1643@ec2-54-247-89-181.eu-west-1.compute.amazonaws.com:5432/d1ngfl7glkmine")
+db = SQL("sqlite:///quiz.db")
 
 
 @app.route("/welcome", methods=["GET", "POST"])
